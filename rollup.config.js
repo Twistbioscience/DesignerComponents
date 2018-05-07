af7bca8009c728fde99986e70e3618cf455a73ed
Eran Lesser <eranlesser@gmail.com>
2018-05-07 11:45:15 +0300

    no message

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
 import resolve from 'rollup-plugin-node-resolve';
 import rootImport from 'rollup-plugin-root-import';
+import serve from 'rollup-plugin-serve'
 
 export default {
   entry: 'src/app/index.js',
@@ -36,6 +37,32 @@ export default {
 
       // If we don't find the file verbatim, try adding these extensions
       extensions: '.js'
+    }),
+    serve({
+      // Launch in browser (default: false)
+      open: true,
+
+      // Show server address in console (default: true)
+      verbose: false,
+
+      // Folder to serve files from
+      contentBase: '',
+
+      // Multiple folders to serve from
+      contentBase: ['build/app', 'static'],
+
+      // Set to true to return index.html instead of 404
+      historyApiFallback: false,
+
+      // Options used in setting up server
+      host: 'localhost',
+      port: 10001,
+
+
+      //set headers
+      headers: {
+        foo: 'bar'
+      }
     }),
     (process.env.NODE_ENV === 'production' && uglify()),
   ],
