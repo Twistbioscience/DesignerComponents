1349414bfc923555674860dd4bc98d3310defe72
Adam Yahid <ayahid@twistbioscience.com>
2018-06-19 10:52:39 +0300

    fix linting

--- src/line-parts/line.js
+++ src/line-parts/line.js
@@ -36,16 +36,7 @@ class Line extends React.Component {
   }
 
   render() {
-    const {
-      charsPerRow,
-      minusStrand,
-      index,
-      style,
-      selection,
-      selectionInProgress,
-      config,
-      startAdapterLength
-    } = this.props;
+    const {charsPerRow, minusStrand, index, style, selection, selectionInProgress, config} = this.props;
     const startIndex = charsPerRow * index;
     const sequence = this.props.sequence.substr(startIndex, charsPerRow).toUpperCase();
     const endIndex = startIndex + sequence.length;
