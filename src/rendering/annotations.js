2a70634ac89710ecd5db149b5ea54db1d9cf42d5
Adam Yahid <ayahid@twistbioscience.com>
2018-06-19 18:08:56 +0300

    make one liner

--- src/rendering/annotations.js
+++ src/rendering/annotations.js
@@ -1,19 +1,18 @@
-const getLayerCount = checkOverlap => (annotations = [], index) => {
-  let newAnno = annotations
+const getLayerCount = checkOverlap => (annotations = [], index) =>
+  annotations
     .slice(0, index)
     .map((annotation, i) => Object.assign({}, annotation, {layer: getLayerCount(checkOverlap)(annotations, i)}))
-    .filter(annotation => checkOverlap(annotations[index], annotation));
-  newAnno.sort((a, b) => {
-    if (a.layer < b.layer) {
-      return -1;
-    }
-    if (b.layer < a.layer) {
-      return 1;
-    }
-    return 0;
-  });
-  return newAnno.reduce((curr, prev) => (prev.layer === curr ? prev.layer + 1 : curr), 1);
-};
+    .filter(annotation => checkOverlap(annotations[index], annotation))
+    .sort((a, b) => {
+      if (a.layer < b.layer) {
+        return -1;
+      }
+      if (b.layer < a.layer) {
+        return 1;
+      }
+      return 0;
+    })
+    .reduce((curr, prev) => (prev.layer === curr ? prev.layer + 1 : curr), 1);
 
 export const getAnnotationLayer = getLayerCount((curr, prev) => curr.startIndex < prev.endIndex);
 export const getResiteLayer = getLayerCount((curr, prev) => curr.startIndex < prev.endIndex);
