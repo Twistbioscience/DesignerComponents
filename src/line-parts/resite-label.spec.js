7d56ce481b100186652adadbbc187b155d7f8498
Andrew Zuckerman <andzuck@gmail.com>
2018-07-03 14:33:25 +0300

    prettier/comment

--- src/line-parts/resite-label.spec.js
+++ src/line-parts/resite-label.spec.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import RestrictionSiteLabel from './resite-label';
 import {render, cleanup} from 'react-testing-library';
-import Enzyme, { shallow } from 'enzyme';
 import {getResiteLayer} from '../rendering/annotations';
 // this add custom jest matchers from jest-dom
 import 'jest-dom/extend-expect';
 
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
-
 const restrictionSites = [
-  {"name": "BsaI", "startIndex": 0, "endIndex": 10, "overhang": 4, "cutIndex3_5": 7, "direction": 1, "color": "#b0b3ee"},
-  {"name": "BsaI", "startIndex": 128, "endIndex": 138, "overhang": 4, "cutIndex3_5": 7, "direction": 1, "color": "#b0b3ee"},
-  {"name": "BsaI", "startIndex": 142, "endIndex": 152, "overhang": 4, "cutIndex3_5": 7, "direction": -1, "color": "#b0b3ee"},
-  {"name": "HaeII", "startIndex": 149, "endIndex": 154, "overhang": -4, "cutIndex3_5": 5, "direction": 1, "color": "#fc957b"},
-  {"name": "EcoRV", "startIndex": 151, "endIndex": 156, "overhang": 0, "cutIndex3_5": 3, "direction": 1, "color": "#7dfc7e"},
-  {"name": "vErYLongStrandwithLongName", "startIndex": 200, "endIndex": 500, "overhang": -50, "cutIndex3_5": 88, "direction": 1, "color": "#800000"},
-  {"name": "EcoRV", "startIndex": 219, "endIndex": 225, "overhang": 0, "cutIndex3_5": 3, "direction": -1, "color": "#7dfc7e"},
-  {"name": "stuff", "startIndex": 240, "endIndex": 265, "overhang": 0, "cutIndex3_5": 3, "direction": -1, "color": "#b0b3ee"},
-  {"name": "yeahhh", "startIndex": 247, "endIndex": 265, "overhang": 4, "cutIndex3_5": 5, "direction": 1, "color": "#7dfc7e"}
+  {name: 'BsaI', startIndex: 0, endIndex: 10, overhang: 4, cutIndex3_5: 7, direction: 1, color: '#b0b3ee'},
+  {name: 'BsaI', startIndex: 128, endIndex: 138, overhang: 4, cutIndex3_5: 7, direction: 1, color: '#b0b3ee'},
+  {name: 'BsaI', startIndex: 142, endIndex: 152, overhang: 4, cutIndex3_5: 7, direction: -1, color: '#b0b3ee'},
+  {name: 'HaeII', startIndex: 149, endIndex: 154, overhang: -4, cutIndex3_5: 5, direction: 1, color: '#fc957b'},
+  {name: 'EcoRV', startIndex: 151, endIndex: 156, overhang: 0, cutIndex3_5: 3, direction: 1, color: '#7dfc7e'},
+  {name: 'vErYLong', startIndex: 200, endIndex: 500, overhang: -50, cutIndex3_5: 88, direction: 1, color: '#800000'},
+  {name: 'EcoRV', startIndex: 219, endIndex: 225, overhang: 0, cutIndex3_5: 3, direction: -1, color: '#7dfc7e'},
+  {name: 'stuff', startIndex: 240, endIndex: 265, overhang: 0, cutIndex3_5: 3, direction: -1, color: '#b0b3ee'},
+  {name: 'yeahhh', startIndex: 247, endIndex: 265, overhang: 4, cutIndex3_5: 5, direction: 1, color: '#7dfc7e'}
 ];
 
+// This is a not a real test right now. Still need to figure out how to render/test svgs.
 describe('resite-label', () => {
   const config = {
     LETTER_FULL_WIDTH_SEQUENCE: 10.703125,
@@ -49,7 +45,7 @@ describe('resite-label', () => {
       maxResiteLayer
     };
 
-    afterEach(cleanup)
+    afterEach(cleanup);
 
     expect(1).toBe(1);
   });
