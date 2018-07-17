17337436d7637f577f95cb41602fa751538a0a9c
Andrew Zuckerman <andzuck@gmail.com>
2018-07-17 13:32:49 +0300

    clean up

--- src/rendering/annotations.js
+++ src/rendering/annotations.js
@@ -41,24 +41,6 @@ const overlapping = (annotation, layerOfAnnotations) => {
   return overlapping;
 };
 
-const getLayerCount = checkOverlap => (annotations = [], index) =>
-  annotations
-    .slice(0, index)
-    .map((annotation, i) => Object.assign({}, annotation, {layer: getLayerCount(checkOverlap)(annotations, i)}))
-    .filter(annotation => checkOverlap(annotations[index], annotation))
-    .sort((a, b) => {
-      if (a.layer < b.layer) {
-        return -1;
-      }
-      if (b.layer < a.layer) {
-        return 1;
-      }
-      return 0;
-    })
-    .reduce((curr, prev) => (prev.layer === curr ? prev.layer + 1 : curr), 1);
-
-export const getOrfLayer = getLayerCount((curr, prev) => curr.orfLineStart < prev.orfLineEnd);
-
 export const filterAnnotations = (annotation, startIndex, charsPerRow) => {
   const showAnnotation =
     (annotation.startIndex <= startIndex && annotation.endIndex >= startIndex) ||
