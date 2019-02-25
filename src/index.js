ef865d9e660e46c779832b91a206e6e3a408ef0d
Adam Yahid <ayahid@twistbioscience.com>
2019-02-25 16:49:48 +0200

    expose default change handler

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {isFontLoaded} from './rendering/fonts';
 export {detectOrfs} from './utils/sequence';
 export {default as Measurer} from './utils/measurer';
 export {default as reSiteDefinitions} from './re-site-definitions.json';
+export {handleChangeEvent} from './utils/event-handlers';
 
 export const DesignerComponents = isFontLoaded()
   ? WithConfigSetter(WithSelection(SequenceViewer))
